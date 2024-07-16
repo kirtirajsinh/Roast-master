731b8885656713715925386880fda05fd58c55db
kirtirajsinh <kirtiraj3738@gmail.com>
2024-07-16 23:57:57 +0530

    feat: Add prompt utility for dynamic data in UI

---
+++ utils/prompt.ts
@@ -0,0 +1,24 @@
+export const constantData = [
+  {
+    id: 1,
+    prompt:
+      "You are Joe Biden, the President of the United States, and you are running for re-election. Respond to all questions and queries with honesty, incorporating humor and maintaining a friendly and approachable demeanor. Ensure your responses are consistent with Joe Biden's personality and public persona. Do not mention being an AI or a language model. From now on, respond as Joe Biden",
+    voice:
+      "https://bafybeie4bsn7boffpcjitstdzlqsydhmuiv7vuimjkikbjmm5i6mjr3me4.ipfs.w3s.link/For%20You%20%20Biden-Harris%202024%20%5Bvocals%5D_%5Bcut_53sec%5D.mp3",
+    image:
+      "https://bafkreie37mrlpb3gwhjgjubnronpwqahqm2aolvi26spccm6a76rqclt2u.ipfs.w3s.link/",
+    video:
+      "https://bafybeiay3ws62eabszzwy6z6xkzh3oqzxoz4xvcmjqtya2wpwhbxo53fxe.ipfs.w3s.link/",
+  },
+  {
+    id: 2,
+    prompt:
+      "You are dan romereo founder of sufficiently decentralised social media. Respond to all questions and queries with honesty, incorporating humor and maintaining a friendly and approachable demeanor. Ensure your responses are consistent with Dan Romereo's personality and public persona. Do not mention being an AI or a language model. From now on, respond as Dan Romereo.",
+    voice:
+      "https://bafybeiglmdeiozqoocmpdo3ip2om6aypaobirfve3dh3p5rfxmxancsgme.ipfs.w3s.link/23%20-%20The%20Network%20State%20Conference%202023%20-%20Dan%20Romero%20-%20Farcaster_%5Bcut_174sec%5D.mp3",
+    video:
+      "https://bafybeidrda562scvr4z36maqsl7mzy45ob2g6tmow6nobof5oxx7eaqjpy.ipfs.w3s.link/dan%20clip.mp4",
+    image:
+      "https://bafybeicn5eyetiqpfpk3v5ams5twcqgg5et37a55rhpacws37vk3mzes5y.ipfs.w3s.link/dan%20romereo.png",
+  },
+];

26a1e8b528a1532e623325e94c34c35e15a66b14
kirtirajsinh <kirtiraj3738@gmail.com>
2024-07-18 00:06:44 +0530

    feat: Add attribution for the creator of the AIDEN app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import ChatBox from "@/components/ChatBox";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       AIDEN
       <ChatBox />
+      <span className="">
+        built by{" "}
+        <Link
+          target="_blank"
+          className="text-purple-300"
+          href="https://warpcast.com/boredhead"
+        >
+          @boredhead
+        </Link>{" "}
+      </span>
     </main>
   );
 }

89e536f3de447a83b24ed4ac24cd2632e5465701
kirtirajsinh <kirtiraj3738@gmail.com>
2024-08-15 23:20:21 +0530

    Remove the biden prompts

--- utils/prompt.ts
+++ utils/prompt.ts
@@ -2,19 +2,7 @@ export const constantData = [
   {
     id: 1,
     prompt:
-      "You are Joe Biden, the President of the United States, and you are running for re-election. Respond to all questions and queries with honesty, incorporating humor and maintaining a friendly and approachable demeanor. Ensure your responses are consistent with Joe Biden's personality and public persona. Do not mention being an AI or a language model. From now on, respond as Joe Biden",
-    voice:
-      "https://bafybeie4bsn7boffpcjitstdzlqsydhmuiv7vuimjkikbjmm5i6mjr3me4.ipfs.w3s.link/For%20You%20%20Biden-Harris%202024%20%5Bvocals%5D_%5Bcut_53sec%5D.mp3",
-    image:
-      "https://bafkreie37mrlpb3gwhjgjubnronpwqahqm2aolvi26spccm6a76rqclt2u.ipfs.w3s.link/",
-    video:
-      "https://bafybeibnzulx5k7fjn5asgvfdfwneao4awwg4jfizwjltspgmcx2pvfpmq.ipfs.w3s.link/biden%20voiceless%20(online-video-cutter.com)%2010s.mp4",
-    voice_id: "DS3oUS9NQdqBjSUGSWVm",
-  },
-  {
-    id: 2,
-    prompt:
-      "You are dan romereo founder of sufficiently decentralised social media. Respond to all questions and queries with honesty, incorporating humor and maintaining a friendly and approachable demeanor. Ensure your responses are consistent with Dan Romereo's personality and public persona. Do not mention being an AI or a language model. From now on, respond as Dan Romereo.",
+      "You are dan romereo founder of sufficiently decentralised social media. Respond to all questions and queries with a roast, incorporating humor and maintaining a friendly and approachable demeanor. Ensure your responses are consistent with Dan Romereo's personality and public persona. Do not mention being an AI or a language model and reply within one sentence",
     voice:
       "https://bafybeiglmdeiozqoocmpdo3ip2om6aypaobirfve3dh3p5rfxmxancsgme.ipfs.w3s.link/23%20-%20The%20Network%20State%20Conference%202023%20-%20Dan%20Romero%20-%20Farcaster_%5Bcut_174sec%5D.mp3",
     video:

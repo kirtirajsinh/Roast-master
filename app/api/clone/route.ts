5be44f7d0b2f54cc4b2551f0bdd821b421196fc1
kirtirajsinh <kirtiraj3738@gmail.com>
2024-08-17 01:13:17 +0530

    refactor: Update clone route to use replicate.predictions.create

--- app/api/clone/route.ts
+++ app/api/clone/route.ts
@@ -6,22 +6,35 @@ export async function POST(req: Request) {
 
   const { face, audio } = body;
   try {
-    const output = await replicate.run(
-      "devxpy/cog-wav2lip:8d65e3f4f4298520e079198b493c25adfc43c058ffec924f2aefc8010ed25eef",
-      {
-        input: {
-          fps: 25,
-          face: face,
-          pads: "0 10 0 0",
-          audio: audio,
-          smooth: true,
-          resize_factor: 1,
-        },
-      }
-    );
-    console.log(output);
+    // const output = await replicate.run(
+    //   "devxpy/cog-wav2lip:8d65e3f4f4298520e079198b493c25adfc43c058ffec924f2aefc8010ed25eef",
+    //   {
+    //     input: {
+    //       fps: 25,
+    //       face: face,
+    //       pads: "0 10 0 0",
+    //       audio: audio,
+    //       smooth: true,
+    //       resize_factor: 1,
+    //     },
+    //   }
+    // );
 
-    return NextResponse.json(output);
+    const response = await replicate.predictions.create({
+      version:
+        "8d65e3f4f4298520e079198b493c25adfc43c058ffec924f2aefc8010ed25eef",
+      input: {
+        fps: 25,
+        face: face,
+        pads: "0 10 0 0",
+        audio: audio,
+        smooth: true,
+        resize_factor: 1,
+      },
+    });
+    console.log(response);
+
+    return NextResponse.json(response?.id);
   } catch (err) {
     console.log(err);
     return NextResponse.json(err);

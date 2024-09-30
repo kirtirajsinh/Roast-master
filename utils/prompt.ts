96b588c16b6efbd883f0db6978e047fe91af74f3
kirtirajsinh <kirtiraj3738@gmail.com>
2024-09-30 21:07:33 +0530

    refactor: Add Joe Rogan prompt to constantData array

--- utils/prompt.ts
+++ utils/prompt.ts
@@ -13,4 +13,18 @@ export const constantData = [
     voice_id: "DS3oUS9NQdqBjSUGSWVm",
     name: "Dan Romereo ",
   },
+  {
+    id: 2,
+    prompt:
+      "You are Joe Rogan famous podcastor. Respond to all questions and queries with a roast, incorporating humor and maintaining a friendly and approachable demeanor. Ensure your responses are consistent with Joe Rogan personality and public persona. Do not mention being an AI or a language model and reply within one sentence",
+    voice:
+      "https://bafybeiglmdeiozqoocmpdo3ip2om6aypaobirfve3dh3p5rfxmxancsgme.ipfs.w3s.link/23%20-%20The%20Network%20State%20Conference%202023%20-%20Dan%20Romero%20-%20Farcaster_%5Bcut_174sec%5D.mp3",
+    video:
+      "https://bafybeib2ijk3afg54yf4pbdp6lt3sa4mnwzjvuzevpvoeeu4ovh7xhxu7y.ipfs.w3s.link/Untitled%20video%20-%20Made%20with%20Clipchamp.mp4",
+    image:
+      "https://bafybeicmxwk3524wi7twsbfrl7nuip5s7ibhvz3ymnbjokajcymjpzsw2y.ipfs.w3s.link/Shouts-Joe-Rogan.webp",
+
+    voice_id: "qjUXlaVM7iBQAfh6yIvV",
+    name: "JOE Rogan ",
+  },
 ];
